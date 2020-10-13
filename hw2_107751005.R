677c32ceafd65d335912f9d10c80fa612f05c46e
sunny70487 <65067286+sunny70487@users.noreply.github.com>
2020-10-13 10:35:36 +0800

    Update hw2_107751005.R

--- hw2_107751005.R
+++ hw2_107751005.R
@@ -30,12 +30,14 @@ print(paste("query mode :", query_m))
 print(paste("output file:", out_f))
 print(paste("files      :", files))
 
-dir = file.path(getwd(),"data","/")
-dir_o=file.path(getwd(),"eval","/")
+dir = file.path(getwd(),"data","/") #data path
+dir_o=file.path(getwd(),"eval","/") #output path
+#set null list
 d <- list()
-S <- 0
+s <- 0
 j <- list()
 h <- list()
+l <- list()
 pred_label <- list()
 cf <- list()
 tp <- list()
@@ -53,33 +55,33 @@ pseudo_R_squared <- list()
 mydata <- list()
 df <- data.frame()
 for(i in files){
-  h[[i]]<-gsub(".csv", "", basename(i))
-  d[[i]] <- read.csv(paste(dir,i,sep=""))
-  j[[i]] <- ifelse(d[[i]]$reference==query_m,1,0)
-  pred_label[[i]] <- as.integer(d[[i]]$pred.score >thre)
-  cf[[i]] <- table(pred_label[[i]], j[[i]])
+  h[[i]]<-gsub(".csv", "", basename(i)) #for index name
+  d[[i]] <- read.csv(paste(dir,i,sep="")) #read specific file
+  j[[i]] <- ifelse(d[[i]]$reference==query_m,1,0) #transformation reference for numeric
+  pred_label[[i]] <- as.integer(d[[i]]$pred.score >thre) #set threthold
+  cf[[i]] <- table(pred_label[[i]], j[[i]]) #confusion matrix
   tp[[i]] <- cf[[i]][2, 2]
   tn[[i]] <- cf[[i]][1, 1]
   fp[[i]] <- cf[[i]][2, 1]
   fn[[i]] <- cf[[i]][1, 2]
-  precision[[i]] <- (tp[[i]] )/(tp[[i]] + fp[[i]])
-  sensitivity[[i]] <- tp[[i]]/(tp[[i]] + fn[[i]])
-  specificity[[i]] <- tn[[i]]/(tn[[i]] + fp[[i]])
-  f1_score[[i]] <- (2*precision[[i]]*sensitivity[[i]])/(precision[[i]]+sensitivity[[i]])
-  logli_model[[i]] <- sum(ifelse(d[[i]]$reference==query_m,log(d[[i]]$pred.score),log(1-d[[i]]$pred.score)))
-  pNull[[i]] <- sum(j[[i]])/dim(d[[i]])[[1]]
-  logli_nullModel[[i]] <- sum(j[[i]])*log(pNull[[i]]) + sum(ifelse(d[[i]]$reference==query_m,0,1))*log(1-pNull[[i]])
-  pseudo_R_squared[[i]] <- 1-(-2*(logli_model[[i]]-S))/(-2*(logli_nullModel[[i]]-S))
-  mydata[[i]] <- data.frame(h[[i]],round(sensitivity[[i]],2),round(specificity[[i]],2),round(f1_score[[i]],2),round(logli_model[[i]],2),round(pseudo_R_squared[[i]],2))
-  names(mydata[[i]]) <- c("method","sensitivity","specificity","F1","Loglikelihood","pseudoR2")
-  df <- rbind(df, mydata[[i]])
+  precision[[i]] <- (tp[[i]] )/(tp[[i]] + fp[[i]]) #precision
+  sensitivity[[i]] <- tp[[i]]/(tp[[i]] + fn[[i]]) #sensitivity(recall)
+  specificity[[i]] <- tn[[i]]/(tn[[i]] + fp[[i]]) #specificity
+  f1_score[[i]] <- (2*precision[[i]]*sensitivity[[i]])/(precision[[i]]+sensitivity[[i]]) #F1_score
+  logli_model[[i]] <- sum(ifelse(d[[i]]$reference==query_m,log(d[[i]]$pred.score),log(1-d[[i]]$pred.score))) #loglikelihod
+  pNull[[i]] <- sum(j[[i]])/dim(d[[i]])[[1]] #caculate null model probability
+  logli_nullModel[[i]] <- sum(j[[i]])*log(pNull[[i]]) + sum(ifelse(d[[i]]$reference==query_m,0,1))*log(1-pNull[[i]]) #null model likelihood
+  pseudo_R_squared[[i]] <- 1-((-2*(logli_model[[i]]-s))/(-2*(logli_nullModel[[i]]-s))) #pseudo_R_squared
+  mydata[[i]] <- data.frame(h[[i]],round(sensitivity[[i]],2),round(specificity[[i]],2),round(f1_score[[i]],2),round(logli_model[[i]],2),round(pseudo_R_squared[[i]],2)) #set dataframe input above value
+  names(mydata[[i]]) <- c("method","sensitivity","specificity","F1","Loglikelihood","pseudoR2") #rename dataframe index
+  df <- rbind(df, mydata[[i]]) #combine each other
 }
-l <- list()
+#compare each column which is better method
 for(s in 2:length(df)){
   l[[s]] <- df[which.max(df[[s]]),][1]
 }
 k <- data.frame(c("max",l[[2]],l[[3]],l[[4]],l[[5]],l[[6]]))
 names(k) <- c("method","sensitivity","specificity","F1","Loglikelihood","pseudoR2")
 df <- rbind(df,k)
-write.csv(df,file=paste(dir_o,out_f,sep=""),row.names=FALSE)
-read.csv(paste(dir_o,out_f, sep=""), header=T)
+write.csv(df,file=paste(dir_o,out_f,sep=""),row.names=FALSE) #write output file
+read.csv(paste(dir_o,out_f, sep=""), header=T) #show result
